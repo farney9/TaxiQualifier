f61023f2bff91aebcf6fda35905dfcf6c14d8518
Farney Jiménez Toro <farney9@gmail.com>
2020-03-02 15:31:14 -0500

    Navigate Another Page TripDetailPage

--- Taxi.Prism/Taxi.Prism/ViewModels/TaxiHistoryPageViewModel.cs
+++ Taxi.Prism/Taxi.Prism/ViewModels/TaxiHistoryPageViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Navigation;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Taxi.Common.Models;
 using Taxi.Common.Services;
@@ -8,16 +10,19 @@ namespace Taxi.Prism.ViewModels
 {
     public class TaxiHistoryPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
         private readonly IApiService _apiService;
         private TaxiResponse _taxi;
         private DelegateCommand _checkPlaqueCommand;
         private bool _isRunning;
         private bool _isShowStars;
+        private List<TripItemViewModel> _trips;
 
         public TaxiHistoryPageViewModel(
             INavigationService navigationService,
             IApiService apiService) : base(navigationService)
         {
+            _navigationService = navigationService;
             _apiService = apiService;
             Title = "Taxi History";
         }
@@ -33,6 +38,7 @@ namespace Taxi.Prism.ViewModels
         public DelegateCommand CheckPlaqueCommand => _checkPlaqueCommand ?? (_checkPlaqueCommand = new DelegateCommand(CheckPlaqueAsync));
 
         public bool IsRunning { get => _isRunning; set => SetProperty(ref _isRunning, value); }
+        public List<TripItemViewModel> Trips { get => _trips; set => SetProperty(ref _trips, value); }
         public bool IsShowStars { get => _isShowStars; set => SetProperty(ref _isShowStars, value); }
 
         private async void CheckPlaqueAsync()
@@ -82,6 +88,23 @@ namespace Taxi.Prism.ViewModels
             }
 
             Taxi = (TaxiResponse)response.Result;
+            Trips = Taxi.Trips.Where(t => t.Qualification != 0).Select(t => new TripItemViewModel(_navigationService)
+            {
+                EndDate = t.EndDate,
+                Id = t.Id,
+                Qualification = t.Qualification,
+                Remarks = t.Remarks,
+                Source = t.Source,
+                SourceLatitude = t.SourceLatitude,
+                SourceLongitude = t.SourceLongitude,
+                StartDate = t.StartDate,
+                Target = t.Target,
+                TargetLatitude = t.TargetLatitude,
+                TargetLongitude = t.TargetLongitude,
+                TripDetails = t.TripDetails,
+                User = t.User
+            }).OrderByDescending(t => t.StartDate).ToList();
+
         }
     }
 }
