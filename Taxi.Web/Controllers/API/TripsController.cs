0280563c9a32407f4da1d8e4169ae175ddfcb627
Farney Jiménez Toro <farney9@gmail.com>
2020-07-05 10:38:59 -0500

    API GetTripEntity, AddTripDetails,  DeleteTripEntity

--- Taxi.Web/Controllers/API/TripsController.cs
+++ Taxi.Web/Controllers/API/TripsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Taxi.Common.Models;
 using Taxi.Web.Data;
@@ -27,6 +28,90 @@ namespace Taxi.Web.Controllers.API
             _userHelper = userHelper;
             _converterHelper = converterHelper;
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTripEntity([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TripEntity tripEntity = await _context.Trips
+                .Include(t => t.TripDetails)
+                .FirstOrDefaultAsync(t => t.Id == id);
+            if (tripEntity == null)
+            {
+                return BadRequest("Trip not found.");
+            }
+
+            return Ok(_converterHelper.ToTripResponse(tripEntity));
+        }
+
+        [HttpPost]
+        [Route("AddTripDetails")]
+        public async Task<IActionResult> AddTripDetails([FromBody] TripDetailsRequest tripDetailsRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (tripDetailsRequest.TripDetails == null || tripDetailsRequest.TripDetails.Count == 0)
+            {
+                return NoContent();
+            }
+
+            TripEntity trip = await _context.Trips
+                .Include(t => t.TripDetails)
+                .FirstOrDefaultAsync(t => t.Id == tripDetailsRequest.TripDetails.FirstOrDefault().TripId);
+            if (trip == null)
+            {
+                return BadRequest("Trip not found.");
+            }
+
+            if (trip.TripDetails == null)
+            {
+                trip.TripDetails = new List<TripDetailEntity>();
+            }
+
+            foreach (TripDetailRequest tripDetailRequest in tripDetailsRequest.TripDetails)
+            {
+                trip.TripDetails.Add(new TripDetailEntity
+                {
+                    Date = DateTime.UtcNow,
+                    Latitude = tripDetailRequest.Latitude,
+                    Longitude = tripDetailRequest.Longitude
+                });
+            }
+
+            _context.Trips.Update(trip);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTripEntity([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var tripEntity = await _context.Trips
+                .Include(t => t.TripDetails)
+                .FirstOrDefaultAsync(t => t.Id == id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(tripEntity);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
 
         [HttpPost]
         [Route("CompleteTrip")]
